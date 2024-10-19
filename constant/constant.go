09c3eb369140ed56c51ceaf28cf92c09b6402325
poteto0 <mahiro.fukuda.aries@gmail.com>
2024-10-19 22:42:47 +0900

    add camara security helmet

--- constant/constant.go
+++ constant/constant.go
@@ -1,12 +1,22 @@
 package constant
 
 const (
-	HEADER_CONTENT_TYPE          string = "Content-Type"
-	APPLICATION_JSON             string = "application/json"
+	MAX_DOMAIN_LENGTH int    = 255
+	PARAM_PREFIX      string = ":"
+	PARAM_TYPE_PATH   string = "path"
+)
+
+// Header
+const (
 	HEADER_ACCESS_CONTROL_ORIGIN string = "Access-Control-Allow-Origin"
 	HEADER_ORIGIN                string = "Origin"
 	HEADER_VARY                  string = "vary"
-	MAX_DOMAIN_LENGTH            int    = 255
-	PARAM_PREFIX                 string = ":"
-	PARAM_TYPE_PATH              string = "path"
+	HEADER_CONTENT_TYPE          string = "Content-Type"
+	APPLICATION_JSON             string = "application/json"
+	CONTENT_SECURITY_POLICY      string = "Content-Security-Policy"
+	X_FRAME_OPTION               string = "X-Frame-Options"
+	STRICT_TRANSPORT_SECURITY    string = "Strict-Transport-Security"
+	X_DOWNLOAD_OPTION            string = "X-Download-Options"
+	X_CONTENT_TYPE_OPTION        string = "X-Content-Type-Options"
+	REFERRER_POLICY              string = "Referrer-Policy"
 )
