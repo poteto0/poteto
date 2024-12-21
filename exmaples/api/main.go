a0d77c2633e5f73d2ed0e76f7d4185032f9abebf
poteto0 <mahiro.fukuda.aries@gmail.com>
2024-12-21 13:26:43 +0900

    EXAMPLE: api, fast-api, jsonrpc

---
+++ exmaples/api/main.go
@@ -0,0 +1,38 @@
+
+package main
+
+import (
+	"net/http"
+
+	"github.com/poteto0/poteto"
+	"github.com/poteto0/poteto/middleware"
+)
+
+func main() {
+	p := poteto.New()
+
+	// Security Header
+	p.Register(middleware.CamaraWithConfig(middleware.DefaultCamaraConfig))
+
+	// CORS
+	p.Register(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+	}))
+
+	p.GET("/", func(ctx poteto.Context) error {
+		return ctx.JSON(http.StatusOK, "Poteto Simple Web framework")
+	})
+
+	p.Leaf("/users", func(userApi poteto.Leaf) {
+		userApi.GET("/", func(ctx poteto.Context) error {
+			return ctx.JSON(http.StatusOK, "get users")
+		})
+		userApi.GET("/:id", func(ctx poteto.Context) error {
+			id, _ := ctx.PathParam("id")
+			return ctx.JSON(http.StatusOK, id)
+		})
+	})
+
+	p.Run(":8000")
+}
